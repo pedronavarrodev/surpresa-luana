0568a8923fecfa8acb233a63d829921b562df50f
Pedro Navarro <54799795+pedronavarrodev@users.noreply.github.com>
2023-04-30 18:25:11 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 29, 2023',
+  birthdate: 'Apr 29, 2024',
   name: 'LUANA'
 };
 
